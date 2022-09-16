9c8423c2167ea05db5d6ca32ea4ebdc0f2a1e37e
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2022-09-15 22:26:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# Créé par Jacques-Conrad Calagui-Painchaud, 2022/09/14
-# Ce programme va vous demander d'écrire une chaine et retournez le nombre de mots
+# Créé par Jacques-Conrad Calagui-Painchaud
+# Créé le 2022/09/14
+# Ce programme va retourner le nombre de dans une chaine fourni par l'usager.
 # TP1
 
 def count_word():
     chain = input("Entrez une chaine:")
     number_of_words = str(len(chain.split(" ")))
-    print(f"Le nombre de mots dans cette chaine est {number_of_words})
+    print(f"Il y a {number_of_words} mots dans cette chaine.")
 
 count_word()
